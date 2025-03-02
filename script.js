8ada2049244c1935b51058055355e2c1cfe2d8d4
Roshan-h978 <roshanrawal398@gmail.com>
2025-03-02 16:19:23 +0545

    election

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+    
+    links.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            link.style.color = "#ffcc00";
+        });
+
+        link.addEventListener("mouseout", () => {
+            link.style.color = "white";
+        });
+    });
+});
